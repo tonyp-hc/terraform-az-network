a4ff2a80b690b27b0b5d70b99c2f8e01f42139d4
Tony Pulickal <ttp@hashicorp.com>
2022-03-13 13:54:25 -0400

    update for TF 0.13

--- main.tf
+++ main.tf
@@ -1,14 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "ttp-rg" {
+  name     = "${var.prefix}-rg"
+  location = var.region
+}
+
 module "network" {
   source              = "app.terraform.io/TonyPulickal/network/azurerm"
   vnet_name           = "${var.prefix}-vnet-tfe"
-  resource_group_name = "${var.prefix}-rg-tfe"
-  location            = "${var.region}"
+  resource_group_name = "${var.prefix}-rg"
+  location            = var.region
   address_space       = "172.16.0.0/16"
   subnet_prefixes     = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   tags = {
-    environment = "${var.env}"
-    owner       = "${var.owner}"
+    environment = var.env
+    owner       = var.owner
   }
+
+  depends_on = [azurerm_resource_group.ttp-rg]
 }
