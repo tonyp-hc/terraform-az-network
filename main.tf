62d69c0d340eaf7cf688139b8ae704c61b2dcd34
Tony Pulickal <ttp@hashicorp.com>
2022-03-13 14:01:33 -0400

    remove legacy params

--- main.tf
+++ main.tf
@@ -18,9 +18,7 @@ resource "azurerm_resource_group" "ttp-rg" {
 
 module "network" {
   source              = "app.terraform.io/TonyPulickal/network/azurerm"
-  vnet_name           = "${var.prefix}-vnet-tfe"
   resource_group_name = "${var.prefix}-rg"
-  location            = var.region
   address_space       = "172.16.0.0/16"
   subnet_prefixes     = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
